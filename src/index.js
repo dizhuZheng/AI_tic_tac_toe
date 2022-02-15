0456c7125ee99a5d21d297904631ae84e7850e16
dizhu <dizhu210@gmail.com>
2022-02-15 18:55:24 -0500

    initialize board

--- src/index.js
+++ src/index.js
@@ -4,13 +4,61 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        { this.props.value }
+      </button>
+    );
+  }
+}
 
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+  render() {
+    const status = 'Next player: X';
+    return (
+      <div>
+        <div className="status">{ status }</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-borad">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{ }</div>
+          <ol>{ }</ol>
+        </div>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Game />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
